40ab85b30249779b1b672c37899c1b22fd2f8264
IOTAIROBERTO <124071310+IOTAIROBERTO@users.noreply.github.com>
2023-03-06 17:57:07 -0700

    .code updates

---
+++ Fecha_POO/Fecha_POO.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Fecha
+{
+    class Fecha
+    {
+        public void PrintFecha()
+        {
+            DateTime fecha = DateTime.Now;
+            Console.WriteLine("Hoy es {0} del mes {1} de {2}",fecha.Day, fecha.Month, fecha.Year);
+            DateTime manyana = fecha.AddDays(1);
+            Console.WriteLine("Mañana será {0}",manyana.Day);
+        } 
+
+        public static void Main(string[] args)
+        {
+            Fecha m_printfecha = new Fecha();
+            m_printfecha.PrintFecha();
+        }
+    }
+}
---
+++ HelloWorld_POO/HelloWorld_POO.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorld_POO
+{
+    class HelloWorld_POO
+    {
+        public void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void Main(string[] args)
+        {
+            HelloWorld_POO m_helloworldpoo = new HelloWorld_POO();
+            m_helloworldpoo.PrintHelloWorld();
+        }
+    }
+}
