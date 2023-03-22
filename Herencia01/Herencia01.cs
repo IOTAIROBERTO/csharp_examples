0fba8effd44732d30d7fc486b18b5297a891cf70
CloudPromavsa <95986505+CloudPromavsa@users.noreply.github.com>
2023-03-21 21:29:19 -0700

    .

--- Herencia01/Herencia01.cs
+++ Herencia01/Herencia01.cs
@@ -2,28 +2,27 @@
 {
     public class Animal
     {
-        public virtual void Greet()
+        public void Greet()
         {
-            Console.WriteLine("Hola, soy algun tipo de animal.");
-        } 
+            Console.WriteLine("Hola, soy un tipo de animal.");
+        }
     }
 
     public class Dog : Animal
     {
-        public override void Greet()
+        public void Greet2()
         {
             Console.WriteLine("Hola, soy un perro.");
-        } 
+        }
     }
 
     public class CallAnimals
-    { 
+    {
         public static void Main(string[] args)
         {
-            Animal animal = new Animal();
-            animal.Greet();
             Dog dog = new Dog();
             dog.Greet();
+            dog.Greet2();
         }
-    }
+    } 
 }
